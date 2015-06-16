3ec00b1626c6ebde25076e2a8169f3a278cb4919
adammhaile <adammhaile@gmail.com>
2015-06-15 20:56:53 -0400

    mapgen

---
+++ js/bp.js
@@ -0,0 +1,42 @@
+// def mapGen(width, height, serpentine = True, offset = 0, rotation = MatrixRotation.ROTATE_0, vert_flip=False):
+//     """Helper method to generate X,Y coordinate maps for strips"""
+
+//     result = []
+//     for y in range(height):
+//         if not serpentine or y % 2 == 0:
+//             result.append([(width * y) + x + offset for x in range(width)])
+//         else:
+//             result.append([((width * (y+1)) - 1) - x + offset for x in range(width)])
+
+//     for i in range(rotation):
+//             result = zip(*result[::-1])
+
+//     if vert_flip:
+//             result = result[::-1]
+
+//     return result
+
+// class MultiMapBuilder():
+//     def __init__(self):
+//         self.map = []
+//         self.offset = 0
+
+//     def addRow(self, *maps):
+//         yOff = len(self.map)
+//         lengths = [len(m) for m in maps]
+//         h = max(lengths)
+//         if(min(lengths) != h):
+//             raise ValueError("All maps in row must be the same height!")
+
+//         offsets = [0 + self.offset]
+//         count = 0
+//         for m in maps:
+//             offsets.append(h*len(m[0]) + offsets[count])
+//             count += 1
+
+//         for y in range(h):
+//             self.map.append([])
+//             for x in range(len(maps)):
+//                 self.map[y + yOff] += [i + offsets[x] for i in maps[x][y]]
+
+//         self.offset = offsets[len(offsets)-1]

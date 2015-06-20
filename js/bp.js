3e54767ecaea09c29506a0e42265c2e6bfaa6638
adammhaile <adammhaile@gmail.com>
2015-06-19 20:22:27 -0400

    working on map builder

--- js/bp.js
+++ js/bp.js
@@ -99,27 +99,3 @@ function MultiMapBuilder(){
         this.offset = offsets[offsets.length-1]
     }
 }
-// class MultiMapBuilder():
-//     def __init__(self):
-//         self.map = []
-//         self.offset = 0
-
-//     def addRow(self, *maps):
-//         yOff = len(self.map)
-//         lengths = [len(m) for m in maps]
-//         h = max(lengths)
-//         if(min(lengths) != h):
-//             raise ValueError("All maps in row must be the same height!")
-
-//         offsets = [0 + self.offset]
-//         count = 0
-//         for m in maps:
-//             offsets.append(h*len(m[0]) + offsets[count])
-//             count += 1
-
-//         for y in range(h):
-//             self.map.append([])
-//             for x in range(len(maps)):
-//                 self.map[y + yOff] += [i + offsets[x] for i in maps[x][y]]
-
-//         self.offset = offsets[len(offsets)-1]
